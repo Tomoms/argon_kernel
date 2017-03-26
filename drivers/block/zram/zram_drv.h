49c54254d99cfd5fd3c5f5dce95cd62e9792bbea
Shilin Victor <chrono.monochrome@gmail.com>
2017-03-26 08:21:31 +0200

    zram: make max_zpage_size tunable
    Signed-off-by: Shilin Victor <chrono.monochrome@gmail.com>

--- drivers/block/zram/zram_drv.h
+++ drivers/block/zram/zram_drv.h
@@ -32,7 +32,7 @@ static const unsigned max_num_devices = 32;
  * Pages that compress to size greater than this are stored
  * uncompressed in memory.
  */
-static const size_t max_zpage_size = PAGE_SIZE / 4 * 3;
+extern size_t max_zpage_size;
 
 /*
  * NOTE: max_zpage_size must be less than or equal to:
