5c061e1a215d2d444a480ac8a955ddf20bfa4e2a
Sultanxda <sultanxda@gmail.com>
2017-08-06 12:10:09 -0700

    cpufreq: Export user-set max frequency to other drivers
    Quick and dirty way to grab the user-requested max frequency, since our
    cpufreq's user_policy member does not accurately reflect what the user
    selected.
    
    This assumes the same max frequency is requested by the user for all CPUs.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -467,6 +467,13 @@ static ssize_t show_scaling_cur_freq(
 static int __cpufreq_set_policy(struct cpufreq_policy *data,
 				struct cpufreq_policy *policy);
 
+static atomic_t user_max = ATOMIC_INIT(0);
+
+int cpufreq_get_user_max(void)
+{
+	return atomic_read(&user_max);
+}
+
 /**
  * cpufreq_per_cpu_attr_write() / store_##file_name() - sysfs write access
  */
@@ -495,6 +502,8 @@ static ssize_t store_##file_name					\
 	ret = cpufreq_driver->verify(&new_policy);			\
 	if (ret)							\
 		pr_err("cpufreq: Frequency verification failed\n");	\
+	else								\
+		atomic_set(&user_max, new_policy.max);			\
 									\
 	policy->user_policy.min = new_policy.min;			\
 	policy->user_policy.max = new_policy.max;			\
