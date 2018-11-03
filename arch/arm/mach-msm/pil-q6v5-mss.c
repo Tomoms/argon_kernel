28ab43447e2dc944d6b2fe71430646f5553dfe0b
Tomoms <fondatommaso2@gmail.com>
2018-11-03 12:48:05 +0100

    Revert "msm: pil: Enable/disable irq's on subsystem powerup/shutdown"
    This reverts commit 9e5cca9d9f9d7f1313631174d3cb1305035da182.

--- arch/arm/mach-msm/pil-q6v5-mss.c
+++ arch/arm/mach-msm/pil-q6v5-mss.c
@@ -125,7 +125,7 @@ static int modem_shutdown(const struct subsys_desc *subsys, bool force_stop)
 
 	pil_shutdown(&drv->mba->desc);
 	pil_shutdown(&drv->q6->desc);
-
+	disable_irq(drv->subsys_desc.wdog_bite_irq);
 	return 0;
 }
 
@@ -148,7 +148,7 @@ static int modem_powerup(const struct subsys_desc *subsys)
 	ret = pil_boot(&drv->mba->desc);
 	if (ret)
 		pil_shutdown(&drv->q6->desc);
-
+	enable_irq(drv->subsys_desc.wdog_bite_irq);
 	return ret;
 }
 
