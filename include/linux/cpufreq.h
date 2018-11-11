bba6b0914b032b7e33a151e83ea3b0371ec291e3
Tomoms <fondatommaso2@gmail.com>
2018-11-11 22:27:38 +0100

    cpufreq: expose check_cpuboost() function

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -26,6 +26,7 @@
 /* Print length for names. Extra 1 space for accomodating '\n' in prints */
 #define CPUFREQ_NAME_PLEN (CPUFREQ_NAME_LEN + 1)
 
+extern bool check_cpuboost(int cpu);
 
 /*********************************************************************
  *                     CPUFREQ NOTIFIER INTERFACE                    *
