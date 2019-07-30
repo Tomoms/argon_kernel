61e0ec42c9ebc0a0016ac544fcec0ed64ed4c088
Tomoms <fondatommaso2@gmail.com>
2019-07-30 07:54:50 +0200

    Revert "cpu_input_boost: Go back to user's minimum frequency after boost"
    This reverts commit 54eb1f566c1ee2c786e405a5d02cd8374ad9be9d.

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -169,12 +169,11 @@ static int do_cpu_boost(struct notifier_block *nb,
 	struct cpufreq_policy *policy = data;
 	struct boost_policy *b = boost_policy_g;
 	struct ib_pcpu *pcpu = per_cpu_ptr(b->ib.boost_info, policy->cpu);
-	unsigned int user_min_freq = policy->min;
 
 	if (val != CPUFREQ_ADJUST)
 		return NOTIFY_OK;
 
-	if (!is_driver_enabled(b))
+	if (!is_driver_enabled(b) && policy->min == policy->cpuinfo.min_freq)
 		return NOTIFY_OK;
 
 	if (is_fb_boost_active(b)) {
@@ -199,7 +198,7 @@ static int do_cpu_boost(struct notifier_block *nb,
 		policy->min = min(policy->max,
 				b->ib.freq[policy->cpu ? 1 : 0]);
 	else
-		policy->min = user_min_freq;
+		policy->min = policy->cpuinfo.min_freq;
 
 	return NOTIFY_OK;
 }
